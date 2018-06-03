f567d36b8b3685bc9291dd603c772b8e7c835bb8
Richard Wong <ricwtk@gmail.com>
2018-06-03 11:59:47 +0800

    add basic ui for preferences

--- desktop/js/v-pref.js
+++ desktop/js/v-pref.js
@@ -12,29 +12,24 @@ module.exports = {
         <div class="grow">Preference</div>
         <div class="mdi mdi-24px mdi-close c-hand" @click="toggle"></div>
       </div>
-      <div class="modal-body">
-        body
-
-
-        fdasf
-
-
-
-
-
-        fdsaf
-        fdasf
-        adsf
-        <div style="min-height: 800px"></div>
-
-
-        afdssdf
-
-        fdsa
-        fdas
-
-
-        fdsafas
+      <div class="modal-body v-box p-1">
+        <div class="text-gray mb-1">Editor font</div>
+        <div class="h-box">
+          <select class="form-select grow mr-1"><option>font family</option></select>
+          <select class="form-select mr-1"><option>font weight</option></select>
+          <select class="form-select"><option>font size</option></select>
+        </div>
+        <div class="text-gray mb-1 mt-2">Base font for display</div>
+        <div class="h-box">
+          <select class="form-select grow mr-1"><option>font family</option></select>
+          <select class="form-select"><option>font size</option></select>
+        </div>
+        <div class="text-gray mb-1 mt-2">Code block highlight theme</div>
+        <div class="h-box">
+          <select class="form-select grow"><option>hljs themes</option></select>
+        </div>
+        <div class="text-gray mb-1 mt-2">Custom CSS</div>
+        <textarea class="form-textarea no-resize"></textarea>
       </div>
       <div class="modal-footer">
         <div class="grow"></div>
